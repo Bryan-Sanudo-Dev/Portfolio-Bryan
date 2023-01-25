c4de88b6ba5cca2a2e5a164e2f85931e87ec74c9
bryan <bryandofus3229@gmail.com>
2023-01-25 12:05:13 -0500

    deploy 1

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import arrayDestruct from "@/assets/Portfolio/arrayDestruct.jpg";
-import installNode from "@/assets/Portfolio/installNode.jpg";
-import navbar from "@/assets/Portfolio/navbar.jpg";
-import reactParallax from "@/assets/Portfolio/reactParallax.jpg";
-import reactSmooth from "@/assets/Portfolio/reactSmooth.jpg";
-import reactWeather from "@/assets/Portfolio/reactWeather.jpg";
+import arrayDestruct from "@/assets/portfolio/arrayDestruct.jpg";
+import installNode from "@/assets/portfolio/installNode.jpg";
+import navbar from "@/assets/portfolio/navbar.jpg";
+import reactParallax from "@/assets/portfolio/reactParallax.jpg";
+import reactSmooth from "@/assets/portfolio/reactSmooth.jpg";
+import reactWeather from "@/assets/portfolio/reactWeather.jpg";
 
 const Portfolio = () => {
   const portfolios = [
